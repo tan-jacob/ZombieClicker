73b1e0a8a6ede0bf570e4a46a745b8d72d0e1b78
Jacob Tan <jacobmathewtan@gmail.com>
2020-02-17 13:55:02 -0800

    Added death gif, updated zombie

--- mainscripts.js
+++ mainscripts.js
@@ -1,17 +1,18 @@
 function moveZombie(){
-
+    document.getElementById("zombie").src="images/zombie2.png"
     let width = window.innerWidth;
     let height = window.innerHeight;
     let zom = document.getElementById('zombie');
 
-    zom.style.left = Math.random()*(width - 128) + "px";
-    zom.style.top = Math.random()*(height - 128) + "px";
+    zom.style.left = Math.random()*(width - 100) + "px";
+    zom.style.top = Math.random()*(height - 100) + "px";
     zom.style.margin = 0 + "px";
 }
 
 function clickZombie(){
     score++;
     let scorebox = document.getElementById("score");
+    document.getElementById("zombie").src="images/death.gif";
     scorebox.innerHTML = "Click the zombie to kill it! Your Score: " + score;
 }
 
