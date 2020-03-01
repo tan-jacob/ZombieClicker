e432626dd55f355160668d6204e64041d05af9f7
Jacob Tan <jacobmathewtan@gmail.com>
2020-02-29 23:42:33 -0800

    added end game button

--- js/mainscripts.js
+++ js/mainscripts.js
@@ -1,15 +1,29 @@
-function moveMush(){
+let mushWidth = 170;
+let mushHeight = 120;
+let game;
+let deathTimer = 750;
+let isDying = false;
+let score = 0;
+let mush = document.getElementById("mush");
+let deathsound = document.getElementById("deathSound");
+let endbutton = document.getElementById("endgame");
+let scorebox = document.getElementById("score");
+
+mush.onclick = clickMush;
+endbutton.onclick = endGame;
+
+function moveMush() {
     if (isDying == false) {
-        mush.src="images/mushidle1.gif"
+        mush.src = "images/mushidle1.gif"
         let width = window.innerWidth;
         let height = window.innerHeight;
-        mush.style.left = Math.random()*(width - 170) + "px";
-        mush.style.top = Math.random()*(height - 120) + "px";
+        mush.style.left = Math.random() * (width - mushWidth) + "px";
+        mush.style.top = Math.random() * (height - mushHeight) + "px";
         mush.style.margin = 0 + "px";
     }
 }
 
-function clickMush(){
+function clickMush() {
     if (isDying) {
         return;
     }
@@ -23,28 +37,37 @@ function clickMush(){
     }, deathTimer);
 }
 
-function updateScore(){
+function updateScore() {
     score++;
-    let scorebox = document.getElementById("score");
     scorebox.innerHTML = "Click the mush to mush it! Your Score: " + score;
 
 }
 
-function mushBirth(){
+function mushBirth() {
     moveMush();
     game = setInterval(moveMush, 2000);
 }
 
-function mushDeath(){
-    mush.src="images/mushdeath2.gif";
+function mushDeath() {
+    mush.src = "images/mushdeath2.gif";
     deathsound.play();
 }
 
-let game;
-let deathTimer = 750;
-let isDying = false;
-let score = 0;
-let mush = document.getElementById("mush");
-let deathsound = document.getElementById("deathSound");
+function resetLocation() {
+    mush.src = "images/mushidle1.gif"
+    let width = window.innerWidth;
+    let height = window.innerHeight;
+    mush.style.left = ((width - mushWidth) / 2) + "px";
+    mush.style.top = ((height - mushHeight) / 2) + "px";
+    //mush.style.margin-left =  -60 + "px";
+    //mush.style.margin-top = -85 + "px";
+}
 
-mush.onclick = clickMush;
+function endGame() {
+    clearInterval(game);
+    resetLocation();
+    score = 0;
+    scorebox.innerHTML = "Click the mush to mush it! Your Score: " + score;
+}
+
+
